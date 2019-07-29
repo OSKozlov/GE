4606e8f9a96468a6957979ecb24d899c07c7cddf
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-07-29 21:36:44 +0300

    File processor service init commit

---
+++ FileProcessorService/src/main/java/com/lux/ge/fileprocessor/app/FileProcessorApp.java
@@ -0,0 +1,12 @@
+package com.lux.ge.fileprocessor.app;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class FileProcessorApp {
+
+    public static void main(String[] args) {
+    	SpringApplication.run(FileProcessorApp.class, args);
+    }
+  
+}
