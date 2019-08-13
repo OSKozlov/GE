4c1264af6e3453a2591e4c13710005e6095c0673
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-08-13 11:06:34 +0300

    refactor

--- tseriesservice/src/main/java/com/lux/ge/tseries/config/KafkaConsumerConfig.java
+++ tseriesservice/src/main/java/com/lux/ge/tseries/config/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -12,7 +13,6 @@ import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import com.fasterxml.jackson.databind.deser.std.StringDeserializer;
 import com.lux.ge.tseries.model.TimeseriesData;
 
 @Configuration
