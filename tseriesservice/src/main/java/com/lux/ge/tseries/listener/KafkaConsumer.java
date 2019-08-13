34a6195eaab5b712251828867db6394522257feb
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-08-13 19:57:01 +0300

    logging

--- tseriesservice/src/main/java/com/lux/ge/tseries/listener/KafkaConsumer.java
+++ tseriesservice/src/main/java/com/lux/ge/tseries/listener/KafkaConsumer.java
@@ -10,7 +10,9 @@ public class KafkaConsumer {
 	
 	@KafkaListener(topics = "data-topic", group = "ge-ts-data", containerFactory = "kafkaListenerContainerFactory")
 	public void consumeJson(TimeseriesData data) {
-		System.out.println("Consumed JSON Message: " + data);
+		System.out.println("Consumed JSON Message: " + "guid=" + data.getGuid()  + " timestamp=" + data.getTimestamp() 
+		+ " type=" + data.getType()  + " value=" + data.getValue());
+		
 	}
 
 }
