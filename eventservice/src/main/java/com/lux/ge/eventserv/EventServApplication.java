8ec5db0f06370db05c1336fe25bedcef604913f1
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-08-19 20:29:49 +0300

    event service

---
+++ eventservice/src/main/java/com/lux/ge/eventserv/EventServApplication.java
@@ -0,0 +1,13 @@
+package com.lux.ge.eventserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventServApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventServApplication.class, args);
+	}
+
+}
