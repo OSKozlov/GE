849f2add0a63cefef30d326c713175df105ad257
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-08-21 17:30:40 +0300

    add timestamp to DataFileEvent

--- eventservice/src/main/java/com/lux/ge/eventserv/model/DataFileEvent.java
+++ eventservice/src/main/java/com/lux/ge/eventserv/model/DataFileEvent.java
@@ -1,9 +1,13 @@
 package com.lux.ge.eventserv.model;
 
+import java.sql.Timestamp;
+
 public class DataFileEvent {
 	
 	private int id;
 	
+	private Timestamp timestamp;
+
 	private String topic;
 	
 	private String type;
@@ -15,7 +19,15 @@ public class DataFileEvent {
 	public void setId(int id) {
 		this.id = id;
 	}
+	
+	public Timestamp getTimestamp() {
+		return timestamp;
+	}
 
+	public void setTimestamp(Timestamp timestamp) {
+		this.timestamp = timestamp;
+	}
+	
 	public String getTopic() {
 		return topic;
 	}
