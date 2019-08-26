0cc81ebdd9af15c3a37183da48cdacda26859626
H134122 <H134122@OKozlov1.rdx.lgc.com>
2019-08-26 19:45:25 +0300

    add service Facade

---
+++ facadservice/src/main/java/com/lux/ge/facade/controller/FacadeController.java
@@ -0,0 +1,22 @@
+package com.lux.ge.facade.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class FacadeController {
+	
+	@RequestMapping("/test")
+    @ResponseBody
+    public String test() {
+        return "Test is ok!";
+    }
+
+	@RequestMapping("/healthcheck.html")
+    public ModelAndView healthCheck() {
+		return new ModelAndView("healthcheck");
+    }
+
+}
